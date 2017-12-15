cd0ed68045f8b3a8ee37cd8895420b2763a767a7
songlilei <602240603@qq.com>
2017-12-15 20:08:11 +0800

    添加mock里造的数据

---
+++ server/mock/lessons.js
@@ -0,0 +1,190 @@
+module.exports = {
+  hasMore: true,
+  list: [
+    {
+      id: 1,
+      title: '西双版纳勐泐大佛寺',
+      url: 'http://m4.tuniucdn.com/filebroker/cdn/olb/9f/10/9f1094f3ea6fd1c71e7d199d36d55eea_w160_h90_c1_t0.jpg',
+      url2:'http://m.tuniucdn.com/filebroker/cdn/olb/9f/10/9f1094f3ea6fd1c71e7d199d36d55eea_w500_h280_c1_t0.jpg',
+      intro:'景飘佛寺”是傣族历史上一位名叫拨龙的傣王为纪念病故的王妃南纱维扁而修建',
+      sell:'已售65万',
+      price: '¥50元',
+      content:' 勐泐大佛寺位于云南省西双版纳傣族自治州州府景洪市城郊，按照国家AAAA级景区标准打造。勐泐大佛寺是在古代傣王朝的皇家寺院景飘佛寺的原址上恢复重建的，景飘佛寺是傣族历史上一位名叫拨龙的傣王为纪念病故的王妃南纱维扁而修建。王妃一生信奉佛法，所以每逢节日傣王就亲临寺院，举行大型法会，以纪念爱妃同时弘扬佛法。据史料记载：“景飘佛寺”始建于明代，是南传佛教象征十二版建筑之一，也是版纳佛教活动的重要场所。该建筑在佛历2883年（即公元1848年，清咸丰年间）战争中被毁，距今已有169年。佛寺于2005年开始重建，于同年3月9日举行了大型的土地开光仪式，于2005年5月9日举行了盛大的大殿奠基开光仪式，在大殿施工时，挖掘出大量的银币、银盒、佛教法事用品等。'
+    },
+    {
+      id: 2,
+      title: '香港太平山缆车',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/3B/2F/Cii-TlewLCiITOFtAAn76jtruUYAABNVAB5YbgACfwC819_w96_h62_c1_t0.jpg',
+      url2:'http://m.tuniucdn.com/fb2/t1/G2/M00/3B/2F/Cii-TFewLCmIJxe3ABL4xOz6w6YAABNVACM8O8AEvjc608_w500_h280_c1_t0.jpg',
+      intro:'香港太平山是香港岛最高的山，海拔达552米',
+      sell:'已售90万',
+      price: '¥32元',
+      content:' 香港太平山是香港岛最高的山，海拔达552米，高居临下，视野开阔。远于19世纪初，太平山顶已经成为货船进入港口的航道地标。而鑑于香港气候炎热潮湿，太平山顶亦顺理成章成为不少达官贵人的避暑胜地。约1868年，港督麦当奴爵士为享受太平山顶清凉宜人的居住环境，特别选址该处兴建避暑别墅。渐渐地，移居太平山成为富豪显赫与名流绅士所推崇的风尚。当时，官爷小姐们要往返山顶，就会聘用穿著整齐制服的轿夫所抬的人力轿车作为交通工具。1904年，居住太平山被正式列为上流社会以及外国使节的专有权利，更见山顶的非凡地位。这个制度，一直沿用至1947年方被废弃。太平山顶一直保持她远离尘嚣的憩静安逸，直至1881年才有突破性发展。当年，一位属于苏格兰高原铁路工作的亚历山大．芬梨．史密夫，向当时的港督轩尼诗爵士提出建议，建设港岛缆车铁路，其中一段连接美利楼南侧至山顶维多利亚峡，即今日的山顶缆车。自19世纪开始，太平山顶渐渐成为上流社会的聚居地。'
+    },
+    {
+      id: 3,
+      title: '澳门威尼斯人贡多拉游船',
+      url: 'http://m.tuniucdn.com/fb2/t1/G3/M00/2E/59/Cii_JVlfGwiIH9jqAAKI0sNxZyQAACznwF1uSQAAojq283_w96_h62_c1_t0.jpg',
+      url2:'http://m.tuniucdn.com/fb2/t1/G3/M00/2E/59/Cii_JVlfGwiIH9jqAAKI0sNxZyQAACznwF1uSQAAojq283_w500_h280_c1_t0.jpg',
+        intro:'贡多拉，又译称“刚朵拉”，中国译为“凤尾船”',
+      sell:'已售70万',
+      price: '¥65元',
+      content:'贡多拉（义大利语：Gondola ），又译称“刚朵拉”，中国译为“凤尾船”。贡多拉源于意大利威尼斯，是当地特有的和最具代表性的传统划船，船身全漆黑色，由一船夫站在船尾划动。而于大 运河购物中心内的贡多拉意念也源于意大利威尼斯内穿梭不断的贡多拉，是游客乘坐手工精巧、别具威尼斯特色的贡多拉船泛舟于圣路卡运河、马可孛罗运河及大运 河上，伴随威尼斯人酒店的贡多拉船夫优雅美妙的歌声，令旅程更添完满。这是去澳门威尼斯人酒店必备的项目。贡多拉浪漫其乐无穷，又译称刚朵拉，中国译为凤 尾船。大运河购物中心内的大运河、圣路卡运河及马可孛罗运河，都是威尼斯有名的运河，三条运河互不相通，各有约10条的贡多拉船，轮流在各自的航道穿 乘坐手工精巧、别具威尼斯特色的贡多拉船泛舟于圣路卡运河、马可孛罗运河及大运河上，伴随船夫优雅美妙的歌声，令旅程更添完满。'
+    },
+    {
+      id: 4,
+      title: '丽江玉龙雪山',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/olb/2d/76/2d767e1efebc51ee1f543d48184bf11e_w96_h62_c1_t0.jpg',
+      url2:'http://m.tuniucdn.com/filebroker/cdn/olb/2d/76/2d767e1efebc51ee1f543d48184bf11e_w500_h280_c1_t0.jpg',
+        intro:"纳西语称为“吾鲁游翠阁”，意为殉情之地，古时纳西青年男女追求完美爱情的精神家园",
+      sell:"已售78万",
+      price: '¥60元',
+      content:'纳西语称为“吾鲁游翠阁”，意为殉情之地，古时纳西青年男女追求完美爱情的精神家园。在纳西东巴经典《鲁般鲁饶》中，曾把云杉坪描绘成白鹿当耕牛、赤虎当坐骑、雉鸟来报晓、白雪酿美酒、树上结金果、洁净无苍蝇的理想乐园，即纳西族传说中的“玉龙第三国”。蓝月谷的水在流淌过程中因受山体阻挡，形成了四个较大的水面，人称“玉液”湖、“镜潭”湖、“蓝月”湖和“听涛”湖。湖岸四周植被繁茂，远处雪峰映衬。湖水是透明的蓝，近乎凝固的湛蓝中，些许的绿意点缀其间。于湖心四顾，白云连横，浮于山际，倒映在湖面，如梦幻影，疑是仙境。玉龙雪山全山共发育有19条现代冰川，其中最大的一条冰川——“白水一号”冰川（即冰川公园）位于主峰扇子陡正下方，全长2.9公里。冰川的末端是千姿百态的冰塔林，因阳光折射呈现出蓝绿色，人称“绿雪奇峰”。游客须乘坐索道方能到达冰川公园，欣赏这一奇景。'
+    },
+    {
+      id: 5,
+      title: '三亚天涯海角',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/61/E1/Cii-Tljc5OiIHIUdAAOJopjOdJMAAI5-QKtiPkAA4m6646_w96_h62_c1_t0.jpg',
+      url2:'http://m.tuniucdn.com/fb2/t1/G2/M00/61/E1/Cii-Tljc5OiIHIUdAAOJopjOdJMAAI5-QKtiPkAA4m6646_w500_h280_c1_t0.jpg',
+        intro:"天涯海角”一词，早时是用来表达一种异乡的情结",
+      sell:"已售118万",
+      price: '¥60元',
+      content:'天涯海角一词，早时是用来表达一种异乡的情结，经过千百年的积淀，承载了丰富深刻的文化内涵。清雍正年间崖州知州程哲在景区一海滨巨石上题刻了天涯二字，民国时期当地政要又在相邻的巨石上题写了海角二字，使这里成了名副其实的天涯海角。与天涯、海角石相隔不远处的南天一柱巨石，雄峙南海之滨，笑傲惊涛骇浪。天涯海角游览区使人们心中的天涯情结找到了物化的载体，每年四方游人慕名来到他们向往的这块圣地，沿着历史名人雕塑园、笆篱凝霞景区、摩崖石刻群、天涯路等景点尽兴游览，以圆心中走遍天涯海角的梦想。'
+    },
+    {
+      id: 6,
+      title: '三亚鹿回头风景区',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/61/DA/Cii-Tljc4GSISRnkAAITj6sxpaIAAI58wIxYqIAAhOn654_w96_h62_c1_t0.jpg',
+        intro:"鹿回头风景区因有鹿的雕塑而出名",
+      sell:"已售30万",
+      price: '¥22元',
+      content:'react'
+    },
+    {
+      id: 7,
+      title: '张家界玻璃桥',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/CA/09/Cii-T1gNrqOIMt0dAAMcq2zWpQ0AADrRAPDaREAAxzD421_w96_h62_c1_t0.jpg',
+        intro:"张家界大峡谷景区，是世界最高、跨度最长的玻璃桥",
+      sell:"已售217万",
+      price: '¥234元',
+        content:'vue'
+    },
+    {
+      id: 8,
+      title: '西安大唐芙蓉园',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/A6/CF/Cii-Tlht3gWIYlpqABx3qH6G_zcAAGW9wLYZygAHHfA761_w96_h62_c1_t0.jpg',
+        intro:"是中国第一个全方位展示盛唐风貌的大型皇家园林式文化主题公园",
+      sell:"已售47万",
+      price: '¥12元',
+        content:'vue'
+    },
+    {
+      id: 9,
+      title: '哈尔滨冰雪大世界',
+      url: 'http://m.tuniucdn.com/fb2/t1/G1/M00/6A/23/Cii9EVZybUWIUWi8AAISI5xcZPkAABDigMsZh4AAhI7733_w96_h62_c1_t0.jpg',
+        intro:"2004年，哈尔滨冰雪大世界被国家文化部授予全国文化产业示范基地称号",
+      sell:"已售237万",
+      price: '¥310元',
+        content:'vue'
+    },
+    {
+      id: 10,
+      title: '沈阳故宫',
+      url: 'http://m.tuniucdn.com/fb2/t1/G1/M00/CA/5C/Cii9EFjjQOOIQ72nAANbFWvgORIAAJU2AAZGl8AA1st313_w96_h62_c1_t0.jpg',
+        intro:"沈阳故宫是中国现存仅次于北京故宫的完整的皇宫建筑",
+      sell:"已售90万",
+      price: '¥58元',
+        content:'vue'
+    },
+    {
+      id: 11,
+      title: '杭州千岛湖中心湖区',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/olb/69/31/693124c8e6850ec0722827015f214c4e_w96_h62_c1_t0.jpg',
+        intro:"原国家旅游局局长刘毅在登上梅峰观景台忍不住为眼前妙景题下“梅峰奇观”四字横匾",
+      sell:"已售146万",
+      price: '¥95元',
+        content:'vue'
+    },
+    {
+      id: 12,
+      title: '青岛海昌极地海洋世界',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/olb/0d/ab/0dabefe672b342cdde8617ced84c6985_w96_h62_c1_t0.jpg',
+        intro:"青岛极地海洋世界一期工程拥有极地动物表演馆和欢乐剧场两个场馆",
+      sell:"已售83万",
+      price: '¥60元',
+        content:'vue'
+    },
+    {
+      id: 13,
+      title: '晋中乔家大院',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/online/64/c3/64c3bc82_w96_h62_c1_t0.jpg',
+        intro:"被誉为北方民居建筑史上一颗璀璨的明珠",
+      sell:"已售236万",
+      price: '¥18元',
+        content:'vue'
+    },
+    {
+      id: 14,
+      title: '晋中平遥古城',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/online/7d/9e/7d9e97c5_w96_h62_c1_t0.jpg',
+        intro:"是中国仅有的以整座古城申报世界文化遗产获得成功的两座古县城之一",
+      sell:"已售21万",
+      price: '¥1元',
+        content:'vue'
+    },
+    {
+      id: 15,
+      title: '香格里拉县虎跳峡镇',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/snc/fe/47/fe47c156e8d0899290e1408a115f2483_w96_h62_c1_t0.jpg',
+        intro:"虎跳峡，中国最深的峡谷之一",
+      sell:"已售74万",
+      price: '¥65元',
+        content:'vue'
+    },
+    {
+      id: 16,
+      title: '西藏布达拉宫',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/online/0b/d4/bd48990_w96_h62_c1_t0.jpg',
+        intro:"作为西藏的象征，布达拉宫是所有朝圣者心中的圣地",
+      sell:"已售156万",
+      price: '¥108元',
+        content:'vue'
+    },
+    {
+      id: 17,
+      title: '亚龙湾热带天堂森林公园',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/61/DD/Cii-Tljc4tKIaSWqAAJIqRJgWz4AAI59gK1UhYAAkjB376_w96_h62_c1_t0.jpg',
+        intro:"犹如伸展的双臂环抱着天下第一湾亚龙湾",
+      sell:"已售217万",
+      price: '¥150元',
+        content:'vue'
+    },
+    {
+      id: 18,
+      title: '鄂尔多斯成吉思汗陵旅游景区',
+      url: 'http://m.tuniucdn.com/filebroker/cdn/olb/d5/15/d5153f663c1a517895ed5929daf16e52_w96_h62_c1_t0.jpg',
+        intro:"这里绿草如茵，一派草原特有的壮丽景色",
+      sell:"已售212万",
+      price: '¥16元',
+        content:'vue'
+    },
+    {
+      id: 19,
+      title: '厦门胡里山炮台',
+      url: 'http://m.tuniucdn.com/fb2/t1/G2/M00/3E/E5/Cii-TlhFOp2IRbqUAAFxUfx7IZwAAFKHQHA6ggAAXFp509_w96_h62_c1_t0.jpg',
+        intro:"胡里山炮台是中国洋务运动的产物",
+      sell:"已售28万",
+      price: '¥2元',
+        content:'vue'
+    },
+    {
+      id: 20,
+      title: '银川镇北堡西部影视城',
+      url: 'http://m.tuniucdn.com/fb2/t1/G1/M00/23/C6/Cii9EFbf7uSIURwhAAEv4el7UgQAACi_AHnFscAAS_5156_w96_h62_c1_t0.jpg',
+        intro:"有宁夏之旅，必游影城的赞誉",
+      sell:"已售139万",
+      price: '¥50元',
+        content:'vue'
+    }
+  ]
+}
