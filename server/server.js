aae64539d14eed68cd00bea7170dae3438c2762f
songlilei <602240603@qq.com>
2017-12-19 12:19:41 +0800

    修改后台和数据

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(session({
 }))
 
 app.use(function (req,res,next) {
-    res.header('Access-Control-Allow-Origin',"*");
+    res.header('Access-Control-Allow-Origin',"http://localhost:8080");
     res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,DELETE');
     res.header('Access-Control-Allow-Headers','Content-Type,Accept');
     res.header('Access-Control-Allow-Credentials',"true");
